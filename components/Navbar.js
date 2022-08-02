ef1b5b60413e585e643572e78eb84af825b4928f
Pitra Pamungkas <ppamungkas@uplandsoftware.com>
2022-08-02 15:42:30 +0700

    Add mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,12 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenu = () => {
+    setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+  };
+
   return (
     <header className="">
       <div className="pt-2 h-12 flex items-center justify-between max-w-7xl mx-auto px-12">
@@ -23,7 +28,8 @@ function Navbar() {
             </Link>
           </div>
         </div>
-        {/* Top right meneu */}
+
+        {/* Top right menu */}
         <nav className="hidden lg:flex items-center pt-1 gap-5">
           <Link href="/memes">
             <a className="font-semibold">Memes</a>
@@ -37,13 +43,43 @@ function Navbar() {
             </a>
           </Link>
         </nav>
+
         {/* Hamburger button */}
         <div className="lg:hidden">
-          <button className="z-40 lg:hidden focus:outline-none block hamburger">
+          <button
+            onClick={handleMenu}
+            className={`z-40 lg:hidden focus:outline-none block hamburger ${
+              isMenuOpen ? 'open' : ''
+            }`}
+          >
             <span className="hamburger-top"></span>
             <span className="hamburger-middle"></span>
             <span className="hamburger-bottom"></span>
           </button>
+        </div>
+
+        {/* Mobile menu */}
+        <div
+          className={`${
+            isMenuOpen ? 'flex' : 'hidden'
+            } absolute flex-col space-y-8 w-full min-h-screen top-0 left-0 bottom-0 pt-36 pl-12 text-lg text-gray-50 uppercase
+           bg-cyan-900`}
+        >
+          <Link href="/">
+            <a className="hover:text-cyan-500">Feature</a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-cyan-500">Documentation</a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-cyan-500">Pricing</a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-cyan-500">Login</a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-cyan-500">Sign up</a>
+          </Link>
         </div>
       </div>
     </header>
