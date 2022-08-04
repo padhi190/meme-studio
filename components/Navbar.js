53c8c8267d0dd116d2d82ace53dc24fea3582da7
Pitra Pamungkas <ppamungkas@uplandsoftware.com>
2022-08-04 19:50:47 +0700

    add memes placeholder

--- components/Navbar.js
+++ components/Navbar.js
@@ -87,7 +87,7 @@ function Navbar() {
           className={`${
             isMenuOpen ? 'flex' : 'hidden'
           } absolute flex-col space-y-8 w-full min-h-screen top-0 left-0 bottom-0 pt-36 pl-12 text-lg text-gray-50 uppercase
-           bg-cyan-900 opacity-95`}
+           bg-cyan-900 z-30`}
         >
           <Link href="/feature">
             <a className="hover:text-cyan-500" onClick={closeMenu}>
--- pages/memes/[id].js
+++ pages/memes/[id].js
@@ -1,22 +1,43 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
 import Layout from '../../components/Layout';
 import { useCollection } from '../../lib/useCollection';
 import { UserContext } from '../../lib/usercontext';
+import { FaRegTrashAlt, FaPencilAlt, FaDownload } from "react-icons/fa";
 
 function ViewMemePage() {
   const user = useContext(UserContext);
   const router = useRouter();
-  const isValid = user && (user?.uid === router.query.id);
+  const isValid = user && user?.uid === router.query.id;
 
-  const { documents, error } = useCollection(`users/${ router.query.id }/memes`);
+  const { documents, error } = useCollection(`users/${router.query.id}/memes`);
 
   return (
     <Layout>
       {!isValid ? 'Error' : !documents && 'Loading...'}
-      {isValid && documents?.map(doc => (
-        <img src={ doc.img_url } className='h-64' key={doc.id}/>
-      ))}
+      <div className="flex flex-col md:flex-row gap-3 items-center">
+        {isValid &&
+          documents?.map((doc) => (
+            <div className="relative shadow-md" key={doc.id}>
+              <Image src={doc.img_url} width={405} height={256} className='shadow-md'/>
+              <p className="absolute top-3 left-0 right-0 text-white text-4xl text-center mx-auto uppercase">
+                {doc.top_text}
+              </p>
+              <p className="absolute bottom-16 left-0 right-0 text-white text-4xl text-center mx-auto uppercase">
+                {doc.bottom_text}
+              </p>
+              <div className='flex justify-between items-center px-2 h-12 bg-slate-100 -mt-3'>
+                <p>Wednesday, 4 Aug 2022</p>
+                <div className='flex justify-between items-center gap-6'>
+                  <p className='cursor-pointer hover:text-red-600'><FaRegTrashAlt /></p>
+                  <p className='cursor-pointer hover:text-cyan-500'><FaPencilAlt /></p>
+                  <p className='cursor-pointer hover:text-cyan-500'><FaDownload /></p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </Layout>
   );
 }
